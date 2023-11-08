8b16b0e6383319faf595f49209caf96e9ec228c8
Hugo Pobil <hpascual@mrhouston.net>
2023-11-08 21:24:31 +0100

    lab-terminado

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length){
+      return this.items[pos]
+    } else {
+      throw new Error("OutOFBounds")
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedlist")
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedlist")
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      let total_sum = 0
+      for (let item of this.items) {
+        total_sum += item
+      }
+      return total_sum
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      let total_sum = 0
+      for (let item of this.items) {
+        total_sum += item
+      }
+      return total_sum / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
